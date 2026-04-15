f258c2de2bd00ddea267ce734283660f5fe77806
Ramya-madasu <m.ramya24062007@gmail.com>
2026-04-15 10:39:48 +0530

    Add files via upload

---
+++ gcf2.js
@@ -0,0 +1,5 @@
+let marks = 40;
+    marks += 100 ;
+    document.getElementById("result").textContent =
+      "Total Marks: " + marks;
+    document.getElementById("text").innerHTML = "Hello JavaScript";
